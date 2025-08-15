5ccbc77b6f3aad97046205027f88f41b13029e0b
GobikaT <gobikathuyamani@gmail.com>
2025-08-15 21:45:34 +0530

    Time: 0 ms (100%), Space: 40.9 MB (85.43%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int n=stones.length;
+        if(n<=1){
+            return stones[n-1];
+        }
+        while(true){
+            Arrays.sort(stones);
+            if(stones[n-2]==0 || stones[n-1]==0){
+                return stones[n-1];
+            }
+            stones[n-1]=stones[n-1] - stones[n-2];
+            stones[n-2]=0;
+
+        }
+    }
+}
